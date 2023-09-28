c6fe9fe77e37f40b207a6c45d77c356ca11944fc
mb454422 <mb454422@ohio.edu>
2023-09-28 16:04:28 -0400

    first draft

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
+Write a program which will use a while loop to prompt and read the high and low temperatures
+for each day from the user. As you read in the high and low temp for a given day, save the
+highest high you have seen so far, and likewise the lowest low. Allow the user to end the loop
+by entering the sentinel value -100.
+When the user ends the loop, print out the highest and lowest temperature seen so far for the
+month of February.
 
+note: Why do we use a while loop? We want our program to be flexible enough to allow the
+user to enter any number of days' worth of data, and to tell the program loop to stop with a
+sentinel value. Since we don’t know in advance how many days of data they will enter, we use
+an indefinite loop – the while
--- high-low-temp.cc
+++ high-low-temp.cc
@@ -1,8 +1,9 @@
 /**
- *   @file: word-count.cc
+ *   @file: high_temp-low_temp-temp.cc
  * @author: Matthew Boucher
- *   @date: 9/22/23
- *  @brief: A program that counts the number of words in a parigraph.
+ *   @date: 9/28/23
+ *  @brief: A program that recordes the high_tempest and low_tempest 
+ * tempretures in a given time frame.
  */
 
 #include <iostream>
@@ -11,4 +12,28 @@
 using namespace std;
 
 int main()
-{}
+{
+    double high_temp,low_temp,rhigh_temp=0,rlow_temp=1000;
+    int day_count=0;
+
+cout<<"Enter -100 once done!"<<endl;
+    while(high_temp != -100 && low_temp != -100)
+    {
+    cout<<"Enter high_temp: ";
+    cin>> high_temp;
+    cout<<"Enter low_temp: ";
+    cin>> low_temp;
+    
+    if (high_temp >= rhigh_temp)
+        {
+            rhigh_temp = high_temp;
+        }
+        if (low_temp <= rlow_temp && low_temp != -100)
+        {
+            rlow_temp= low_temp;
+        }
+        day_count++;
+    }
+    cout<<"The record high_temp over "<<day_count<<" days was "<<rhigh_temp<<"f"<<endl;
+    cout<<"The record low_temp over "<<day_count<<" days was "<<rlow_temp<<"f"<<endl; 
+}

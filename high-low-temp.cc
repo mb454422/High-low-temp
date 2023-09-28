799d8b472e754f55d801c3119ed7c989dad7aa33
mb454422 <143750415+mb454422@users.noreply.github.com>
2023-09-28 15:16:25 -0400

    Create high-low-temp.cc

---
+++ high-low-temp.cc
@@ -0,0 +1,14 @@
+/**
+ *   @file: word-count.cc
+ * @author: Matthew Boucher
+ *   @date: 9/22/23
+ *  @brief: A program that counts the number of words in a parigraph.
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+int main()
+{}
